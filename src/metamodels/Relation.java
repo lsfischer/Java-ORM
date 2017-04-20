dc433df043f4b30a9f71518b6d2e34b6610020d7
Lucas Fischer <lucasfischerpt@gmail.com>
2017-04-20 11:39:38 +0100

    Completed Relation metamodel

--- src/metamodels/Relation.java
+++ src/metamodels/Relation.java
@@ -6,5 +6,24 @@ package metamodels;
 public class Relation {
     private Class foreignClass;
     private String relationshipType;
-    //teste
+
+    public Relation(Class foreignClass, String relationshipType){
+        this.foreignClass = foreignClass;
+        this.relationshipType = relationshipType;
+    }
+
+    public Class getForeignClass(){
+        return this.foreignClass;
+    }
+    public String getRelationshipType(){
+        return this.relationshipType;
+    }
+    public void setForeignClass(Class foreignClass){
+        this.foreignClass = foreignClass;
+    }
+    public void setRelationshipType(String relationshipType){
+        this.relationshipType = relationshipType;
+    }
+
+
 }
