e383187ea538e59f3935d0b6fb915765f8a15db9
daniel <the.daniel.basilio@gmail.com>
2017-04-20 12:31:54 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -21,11 +21,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-//vou por mas e isto
-        //hi daniel tens a certeza boy?
-        //HELLO LUCASkzbdlvkjnvkjxcv
-
-
+        
         // Build model
         //Model model = getPersonModel();
         //buildModel(model);
