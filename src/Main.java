3aa69a6f39b3a9c747a11d35322fb552db513a76
Lucas Fischer <lucasfischerpt@gmail.com>
2017-04-20 08:47:43 +0100

    Update

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
         //buildModel(model);
         //buildModel(getBookStoreModel());
 
-        // Test ORM asdiasjdoasijd
+        // Test ORM
         //testORM();
         // LAB 6
         Model model = Model2Model.getModel("src/models/bookstore.xml");
