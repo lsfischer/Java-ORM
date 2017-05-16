4420092f58f376860e4effa12c041b2c98774f82
daniel <the.daniel.basilio@gmail.com>
2017-05-16 16:42:25 +0100

    Updates

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,7 @@ public class Main {
     public static void main(String[] args) {
         startProgram();
         //testORM();
+        //ENVIEI ISTO
     }
 
     public static String getModelChoice() {
