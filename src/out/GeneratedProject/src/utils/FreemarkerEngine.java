91d73c5adc27c0c1763c7e163c1d6b0b4cb9f251
mrLucasFischer <lucasfischerpt@gmail.com>
2017-06-16 16:58:00 +0100

    updates

---
+++ src/out/GeneratedProject/src/utils/FreemarkerEngine.java
@@ -0,0 +1,50 @@
+/*
+ * Freemarker template engine
+ *
+ */
+
+/*package utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+public class FreemarkerEngine {
+
+    private Configuration cfg;
+
+    public FreemarkerEngine(String pathname) {
+        try {
+            cfg = new Configuration(Configuration.VERSION_2_3_25); //mudar para version 2_3_26
+            cfg.setDirectoryForTemplateLoading(new File(pathname));
+            cfg.setDefaultEncoding("UTF-8");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Renders a model using a template.
+     *
+     * @param model the model
+     * @param name  the template name
+     * @return      String
+     */
+    /*public String render(Object model, String name) {
+        try {
+            Template template = cfg.getTemplate(name);
+            Writer out = new StringWriter();
+            template.process(model, out);
+            return out.toString();
+        } catch (TemplateException | IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+*/
---
+++ src/out/GeneratedProject/src/utils/SQLiteConn.java
@@ -0,0 +1,69 @@
+/*
+ * SQLite JDBC Connection
+ *
+ */
+/*
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+public class SQLiteConn {
+    private Connection connection;
+
+    public SQLiteConn(String filename) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + filename);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Closes the database connection.
+     */
+    /*public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Executes an INSERT, UPDATE or DELETE sql statement.
+     *
+     * @param sql the sql statement
+     * @return either the id or 0 for statements that
+     *         do not return anything
+     */
+    /*public int executeUpdate(String sql) {
+        try {
+            Statement stmt = connection.createStatement();
+            stmt.executeUpdate(sql);
+            return stmt.getGeneratedKeys().getInt(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * Executes a given sql statement.
+     *
+     * @param sql the sql statement
+     * @return ResultSet with the results
+     */
+    /*public ResultSet executeQuery(String sql) {
+        try {
+            Statement stmt = connection.createStatement();
+            return stmt.executeQuery(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }*/
+//}
